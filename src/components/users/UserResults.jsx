3a7d5571f3d8563ee704839e20553be190d4ccd2
Golam Morshed <golamdotmorshed@gmail.com>
2022-01-04 00:53:19 +0600

    userresult ui grid update

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -8,7 +8,7 @@ function UserResults() {
 
     if (!loading) {
         return (
-            <div className='mt-8 grid grid-cols-2 gap-8 md:grid-cols-3 lg:grid-cols-4'>
+            <div className='mt-8 grid grid-cols-1 gap-8 xl-grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
                 {users.map(user => (
                     <UserItem key={user.id} user={user} />
                 ))}
